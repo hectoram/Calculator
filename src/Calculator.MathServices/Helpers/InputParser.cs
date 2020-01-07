d74dee4a8fd28543bf2e44d28779dc06899afe40
hectoram <hector.martinez.1338@gmail.com>
2020-01-07 11:37:09 -0800

    Requirement #7
    - Add Variable Length Delimiter Support
    - Added unit tests

--- src/Calculator.MathServices/Helpers/InputParser.cs
+++ src/Calculator.MathServices/Helpers/InputParser.cs
@@ -9,9 +9,12 @@ namespace Calculator.MathServices.Helpers
         {
             if(paramString.Contains("//"))
             {
-                return ParseSingleCharDelimiter(paramString);
+                if(paramString.Contains("[") && paramString.Contains("]"))
+                    return ParseMultiCharDelimiter(paramString);
+                else
+                    return ParseSingleCharDelimiter(paramString);
             }
-            
+
             //Simple case of comma and new line parsing
             var parsedArray = paramString.Split(new string[] { ",", @"\n" }, StringSplitOptions.None);
             return parsedArray;
@@ -19,12 +22,20 @@ namespace Calculator.MathServices.Helpers
 
         private string[] ParseSingleCharDelimiter(string paramString)
         {
-            var customDelimiter = paramString.Substring(2, 1)[0].ToString();
+            var customDelimiter = paramString.Substring(2, 1)[0].ToString();            
+            paramString = paramString.Substring(paramString.IndexOf(@"\n") + 2); // Want everything after new line which is two characters.
+                
+            return paramString.Split(new string[] { ",", @"\n", customDelimiter }, StringSplitOptions.None);
+        }
 
-                int index = paramString.IndexOf(@"\n");
-                paramString = paramString.Substring(index + 2); // Want everything after new line which is two characters.
-                    
-                return paramString.Split(new string[] { ",", @"\n", customDelimiter }, StringSplitOptions.None);
+        private string[] ParseMultiCharDelimiter(string paramString)
+        {
+            var startIndex = paramString.IndexOf('[') + 1; // Do not want to include [
+            var endIndex = paramString.IndexOf("]", startIndex);
+            var customDelimiter = paramString.Substring(startIndex, endIndex - startIndex);
+  
+            paramString = paramString.Substring(paramString.IndexOf(@"\n") + 2); // Want everything after new line which is two characters.               
+            return paramString.Split(new string[] { ",", @"\n", customDelimiter }, StringSplitOptions.None);
         }
     }
 }   
--- tests/Calculator.MathServices.UnitTests/InputParserUnitTests.cs
+++ tests/Calculator.MathServices.UnitTests/InputParserUnitTests.cs
@@ -19,6 +19,7 @@ namespace Calculator.MathServices.UnitTests
         [InlineData("50",1)]
         [InlineData("50,3",2)]
         [InlineData(@"50\n3",2)]
+        [InlineData(@"50\n3\n4\n1",4)]
         [InlineData("50,3,4",3)]
         public void Parse_Returns_CorrectCount_For_CommaAndNewLine(string paramString, int expectedCount)
         {
@@ -32,6 +33,17 @@ namespace Calculator.MathServices.UnitTests
         [InlineData(@"//%\n2%5\n3",3)]
         [InlineData(@"//%\n2%5\n3,5",4)]
         public void Parse_Returns_CorrectCount_For_SingleCharDelimiter(string paramString, int expectedCount)
+        {
+            var result = _parser.Parse(paramString);
+            Assert.Equal(expectedCount, result.Length);
+        }
+
+        [Theory]
+        [InlineData(@"//[***]\n11***22***33",3)]
+        [InlineData(@"//[::]\n11::22", 2)]
+        [InlineData(@"//[%]\n2%5%3",3)]
+        [InlineData(@"//[%%]\n2%%5%%3",3)]
+        public void Parse_Returns_CorrectCount_For_MultiCharDelimiter(string paramString, int expectedCount)
         {
             var result = _parser.Parse(paramString);
             Assert.Equal(expectedCount, result.Length);
